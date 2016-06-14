4356260bca38f884854a635fc396346b3f36f0a1
Hans Kuijpers - Hypernode <hans2103@gmail.com>
2016-06-14 07:42:48 +0000

    #1 fix getting score instead of recommends

--- app/code/local/Hans2103/KiyohsnippetsOverride/Helper/Data.php
+++ app/code/local/Hans2103/KiyohsnippetsOverride/Helper/Data.php
@@ -2,6 +2,44 @@
 
 class Hans2103_KiyohsnippetsOverride_Helper_Data extends Magmodules_Kiyohsnippets_Helper_Data {
 
+	function getSnapshopRequest() {
+		$kiyoh_shop_id = Mage::getStoreConfig('kiyohsnippets/api/shop_id');
+		$api_url = Mage::getStoreConfig('kiyohsnippets/api/api_url');
+		$filename = 'https://' . $api_url . '/widgetfeed.php?company=';
+
+ 		if(!$kiyoh_shop_id) {
+   		return false;
+   	}
+
+   	$xml = $this->getXml($filename . $kiyoh_shop_id, 1);
+		if(!$xml) {
+  		return false;
+    }
+
+    $data = $xml->channel->description;
+		$snippets = array();
+
+    $snippets['avg'] = $this->getSnippetsAvg($data);
+    $snippets['qty'] = $this->getSnippetsQty($data);
+    $snippets['shopname'] = 'shopname';
+
+		return $snippets;
+	}
+
+	function getSnippetsAvg($data) {
+
+    $pieces = explode(',',$data);
+    $score  = explode(' ',$pieces[0]);
+
+  	return array_pop($score);
+	}
+
+	function getSnippetsQty($data) {
+  	$pieces = explode(' ', $data);
+
+  	return array_pop($pieces);
+	}
+
   function getKiyohShowLink() {
     return Mage::getStoreConfig('kiyohsnippets/api/show_link');
   }
